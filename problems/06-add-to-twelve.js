c463efa03899a26417767ee1e4f76a8345062a7c
Emilio <emiliof197@gmail.com>
2023-03-10 18:36:01 -0600

    solved 06

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -5,17 +5,24 @@ return false.
 
 Examples:
 
-addToTwelve([1, 3, 4, 7, 5]); // true
-addToTwelve([1, 3, 4, 7, 6]); // false
-addToTwelve([1, 11, 4, 7, 6]); // true
-addToTwelve([1, 12, 4, 7, 6]); // false
-addToTwelve([1]); // false
 ***********************************************************************/
 
 
-function addToTwelve(arr) {
-  // Your code here
+function addToTwelve(arr,  index = 0) {
+
+  if (index >= arr.length -1 ){
+    return false;
+  }
+  if (arr[index] + arr[index + 1]=== 12 ){
+    return true;
+  }
+  return addToTwelve(arr, index + 1);
 }
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
+console.log(addToTwelve([1, 3, 4, 7, 6])); // false
+console.log(addToTwelve([1, 11, 4, 7, 6])); // true
+console.log(addToTwelve([1, 12, 4, 7, 6])); // false
+console.log(addToTwelve([1])); // false
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
